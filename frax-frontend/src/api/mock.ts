4f22bdbba0955b247ec783824cdc7e9db68b73b0
f-player <ekaterina20059t2005@yandex.ru>
2025-11-10 20:59:55 +0300

    name product

--- frax-frontend/src/api/mock.ts
+++ frax-frontend/src/api/mock.ts
@@ -1,40 +1,40 @@
 import type {IPaginatedProducts} from "../types/index.ts";
 
 export const PRODUCTS_MOCK: IPaginatedProducts = {
-  total: 3,
+  total: 5,
   items: [
-{
+    {
       id: 101,
-      title: "Наземные травоядные",
+      title: "Terrestrial Herbivores",
       image: "mock_images/default.webp",
       c_pol: -20,
       n_pol: 5,
     },
     {
       id: 102,
-      title: "C3-злаки",
-      image:"mock_images/default.webp",
+      title: "C3 Cereals",
+      image: "mock_images/default.webp",
       c_pol: -24,
       n_pol: -5,
     },
     {
       id: 103,
-      title: "Морская рыба",
-      image:"mock_images/default.webp",
+      title: "Marine Fish",
+      image: "mock_images/default.webp",
       c_pol: -15,
       n_pol: 5,
     },
-        {
+    {
       id: 104,
-      title: "Пульсы / бобовые",
-      image:"mock_images/default.webp",
+      title: "Pulses / Legumes",
+      image: "mock_images/default.webp",
       c_pol: -30,
       n_pol: 2,
     },
     {
       id: 105,
-      title: "Овощи",
-      image:"mock_images/default.webp",
+      title: "Vegetables",
+      image: "mock_images/default.webp",
       c_pol: -28,
       n_pol: 2,
     },
